49a041feda881bf7b854f7aac76d6bd4edd1a0d5
Jose Andres Solano Vargas <josesolanovarga13@gmail.com>
2024-11-04 21:22:24 -0600

    TOMEN PUTAS

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import LoginIcon from '@mui/icons-material/Login'; // Importar el ícono de Login
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'; // Importar el ícono de carrito
+import LocalBarIcon from '@mui/icons-material/LocalBar'; // Importar el ícono de bebidas
+import FastfoodIcon from '@mui/icons-material/Fastfood'; // Importar el ícono de comida
+import RestaurantIcon from '@mui/icons-material/Restaurant'; // Importar el ícono de cubiertos
+import "./App.css"; // Asegúrate de que la ruta sea correcta
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={createTheme()}>
+      <div className="App">
+        <header>
+          <div className="menu_side_div">
+            <nav className="Menu_sidebar">
+              <h1 className="Titulo">The Grill Steak</h1>
+              <button className="boton_clientes">
+                <LoginIcon style={{ marginRight: '8px' }} /> {/* Espacio entre el ícono y el texto */}
+              </button>
+              <button className="boton_Proveedores">
+                <ShoppingCartIcon style={{ marginRight: '8px' }} /> {/* Espacio entre el ícono y el texto */}
+              </button>
+            </nav>
+          </div>
+        </header>
+        
+        {/* Panel con desplazamiento horizontal */}
+        <div className="panel_scroll">
+          <div className="contenido_panel">
+            {/* Contenido del panel, puede ser cualquier cosa */}
+            <button className="boton_Category">
+            <RestaurantIcon style={{ marginRight: '8px' }} /> {/* Espacio entre el ícono y el texto */}
+              </button>
+
+              <button className="boton_Category2">
+              <LocalBarIcon style={{ marginRight: '8px' }} /> {/* Espacio entre el ícono y el texto */}
+              </button>
+ 
+
+              <button className="boton_Category3">
+              <FastfoodIcon style={{ marginRight: '8px' }} /> {/* Espacio entre el ícono y el texto */}
+              </button>
+
+
+              <button className="boton_Category4"> 
+                <LoginIcon style={{ marginRight: '8px' }} /> {/* Espacio entre el ícono y el texto */}
+              </button>
+
+
+              
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
 
